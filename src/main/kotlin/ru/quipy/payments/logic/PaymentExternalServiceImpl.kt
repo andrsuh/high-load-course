b3523e93a198ee4a192bc08ed64126a34158f468
mullerdaniil <mullerdaniil@mail.ru>
2024-03-15 16:45:17 +0300

    feat: add lab-3

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -2,18 +2,20 @@ package ru.quipy.payments.logic
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import okhttp3.Dispatcher
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
+import okhttp3.*
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import ru.quipy.common.utils.CoroutineRateLimiter
+import ru.quipy.common.utils.NonBlockingOngoingWindow
 import ru.quipy.core.EventSourcingService
 import ru.quipy.payments.api.PaymentAggregate
+import java.io.IOException
+import java.lang.Double.min
 import java.net.SocketTimeoutException
 import java.time.Duration
 import java.util.*
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 
 // Advice: always treat time as a Duration
@@ -35,15 +37,43 @@ class PaymentExternalServiceImpl(
     private val requestAverageProcessingTime = properties.request95thPercentileProcessingTime
     private val rateLimitPerSec = properties.rateLimitPerSec
     private val parallelRequests = properties.parallelRequests
+    private val cost = properties.cost
+
+    private val _rateLimiter = CoroutineRateLimiter(rateLimitPerSec, TimeUnit.SECONDS)
+    override val rateLimiter: CoroutineRateLimiter
+        get() = _rateLimiter
+
+    private val _window = NonBlockingOngoingWindow(parallelRequests)
+    override val window: NonBlockingOngoingWindow
+        get() = _window
+
+    override val getCost: Double
+        get() = cost
 
     @Autowired
     private lateinit var paymentESService: EventSourcingService<UUID, PaymentAggregate, PaymentAggregateState>
 
-    private val httpClientExecutor = Executors.newSingleThreadExecutor()
+    private val httpClientExecutor = Executors.newCachedThreadPool()
 
     private val client = OkHttpClient.Builder().run {
-        dispatcher(Dispatcher(httpClientExecutor))
+        val dis = Dispatcher(httpClientExecutor)
+        dis.maxRequestsPerHost = parallelRequests
+        dis.maxRequests = parallelRequests
+        dispatcher(dis)
         build()
+    }
+
+    override fun getSpeed(): Double {
+        val averageTime = requestAverageProcessingTime.toMillis().toDouble()
+        return min(parallelRequests.toDouble(), rateLimitPerSec.toDouble().times(averageTime) ) / averageTime
+    }
+
+    override fun canWait(paymentStartedAt: Long): Boolean {
+        return paymentOperationTimeout - Duration.ofMillis(now() - paymentStartedAt) >= requestAverageProcessingTime.multipliedBy(2)
+    }
+
+    override fun notOverTime(paymentStartedAt: Long): Boolean {
+        return Duration.ofMillis(now() - paymentStartedAt) + requestAverageProcessingTime < paymentOperationTimeout
     }
 
     override fun submitPaymentRequest(paymentId: UUID, amount: Int, paymentStartedAt: Long) {
@@ -63,40 +93,46 @@ class PaymentExternalServiceImpl(
             post(emptyBody)
         }.build()
 
-        try {
-            client.newCall(request).execute().use { response ->
-                val body = try {
-                    mapper.readValue(response.body?.string(), ExternalSysResponse::class.java)
-                } catch (e: Exception) {
-                    logger.error("[$accountName] [ERROR] Payment processed for txId: $transactionId, payment: $paymentId, result code: ${response.code}, reason: ${response.body?.string()}")
-                    ExternalSysResponse(false, e.message)
-                }
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                when (e) {
+                    is SocketTimeoutException -> {
+                        paymentESService.update(paymentId) {
+                            it.logProcessing(false, now(), transactionId, reason = "Request timeout.")
+                        }
+                    }
 
-                logger.warn("[$accountName] Payment processed for txId: $transactionId, payment: $paymentId, succeeded: ${body.result}, message: ${body.message}")
+                    else -> {
+                        logger.error("[$accountName] Payment failed for txId: $transactionId, payment: $paymentId", e)
 
-                // Здесь мы обновляем состояние оплаты в зависимости от результата в базе данных оплат.
-                // Это требуется сделать ВО ВСЕХ ИСХОДАХ (успешная оплата / неуспешная / ошибочная ситуация)
-                paymentESService.update(paymentId) {
-                    it.logProcessing(body.result, now(), transactionId, reason = body.message)
-                }
-            }
-        } catch (e: Exception) {
-            when (e) {
-                is SocketTimeoutException -> {
-                    paymentESService.update(paymentId) {
-                        it.logProcessing(false, now(), transactionId, reason = "Request timeout.")
+                        paymentESService.update(paymentId) {
+                            it.logProcessing(false, now(), transactionId, reason = e.message)
+                        }
                     }
                 }
+                window.releaseWindow()
+            }
 
-                else -> {
-                    logger.error("[$accountName] Payment failed for txId: $transactionId, payment: $paymentId", e)
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    val body = try {
+                        mapper.readValue(response.body?.string(), ExternalSysResponse::class.java)
+                    } catch (e: Exception) {
+                        logger.error("[$accountName] [ERROR] Payment processed for txId: $transactionId, payment: $paymentId, result code: ${response.code}, reason: ${response.body?.string()}")
+                        ExternalSysResponse(false, e.message)
+                    }
 
+                    logger.warn("[$accountName] Payment processed for txId: $transactionId, payment: $paymentId, succeeded: ${body.result}, message: ${body.message}, duration: ${now() - paymentStartedAt}")
+
+                    // Здесь мы обновляем состояние оплаты в зависимости от результата в базе данных оплат.
+                    // Это требуется сделать ВО ВСЕХ ИСХОДАХ (успешная оплата / неуспешная / ошибочная ситуация)
                     paymentESService.update(paymentId) {
-                        it.logProcessing(false, now(), transactionId, reason = e.message)
+                        it.logProcessing(body.result, now(), transactionId, reason = body.message)
                     }
                 }
+                window.releaseWindow()
             }
-        }
+        })
     }
 }
 
--- src/main/kotlin/ru/quipy/payments/subscribers/OrderPaymentSubscriber.kt
+++ src/main/kotlin/ru/quipy/payments/subscribers/OrderPaymentSubscriber.kt
@@ -6,17 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import ru.quipy.common.utils.NamedThreadFactory
+import ru.quipy.common.utils.NonBlockingOngoingWindow
 import ru.quipy.core.EventSourcingService
 import ru.quipy.orders.api.OrderAggregate
 import ru.quipy.orders.api.OrderPaymentStartedEvent
 import ru.quipy.payments.api.PaymentAggregate
 import ru.quipy.payments.config.ExternalServicesConfig
-import ru.quipy.payments.logic.PaymentAggregateState
-import ru.quipy.payments.logic.PaymentService
-import ru.quipy.payments.logic.create
+import ru.quipy.payments.logic.*
 import ru.quipy.streams.AggregateSubscriptionsManager
 import ru.quipy.streams.annotation.RetryConf
 import ru.quipy.streams.annotation.RetryFailedStrategy
+import java.time.Duration
 import java.util.*
 import java.util.concurrent.Executors
 import javax.annotation.PostConstruct
@@ -32,14 +32,48 @@ class OrderPaymentSubscriber {
     @Autowired
     private lateinit var paymentESService: EventSourcingService<UUID, PaymentAggregate, PaymentAggregateState>
 
+
     @Autowired
-    @Qualifier(ExternalServicesConfig.PRIMARY_PAYMENT_BEAN)
+    @Qualifier(ExternalServicesConfig.FIRST_PAYMENT_BEAN)
     private lateinit var paymentService: PaymentService
+
+    private fun getIndex(): Int {
+        return Random().nextInt(4)
+    }
+
+    private fun isReset() : Boolean {
+        val value = Random().nextDouble()
+        return value <= 0.2
+    }
+
+    @Autowired
+    @Qualifier(ExternalServicesConfig.SECOND_PAYMENT_BEAN)
+    private lateinit var secondPaymentService: PaymentService
+
+    @Autowired
+    @Qualifier(ExternalServicesConfig.THIRD_PAYMENT_BEAN)
+    private lateinit var thirdPaymentService: PaymentService
+
+    @Autowired
+    @Qualifier(ExternalServicesConfig.FOURTH_PAYMENT_BEAN)
+    private lateinit var fourthPaymentService: PaymentService
+
+    @Autowired
+    private lateinit var paymentServices: List<PaymentService>
 
     private val paymentExecutor = Executors.newFixedThreadPool(16, NamedThreadFactory("payment-executor"))
 
+    private var nearestTimes = longArrayOf(Long.MAX_VALUE, Long.MAX_VALUE, Long.MAX_VALUE, Long.MAX_VALUE)
+
+    private fun getNearest() : Int {
+        val minTime = nearestTimes.min()
+        return nearestTimes.indexOf(minTime)
+    }
+
     @PostConstruct
     fun init() {
+        paymentServices = paymentServices.sortedBy { it.getCost }
+
         subscriptionsManager.createSubscriber(OrderAggregate::class, "payments:order-subscriber", retryConf = RetryConf(1, RetryFailedStrategy.SKIP_EVENT)) {
             `when`(OrderPaymentStartedEvent::class) { event ->
                 paymentExecutor.submit {
@@ -52,7 +86,41 @@ class OrderPaymentSubscriber {
                     }
                     logger.info("Payment ${createdEvent.paymentId} for order ${event.orderId} created.")
 
-                    paymentService.submitPaymentRequest(createdEvent.paymentId, event.amount, event.createdAt)
+                    outerCycle@ while(true) {
+                        if (!isReset()) {
+                            val index = getNearest()
+                            if (paymentServices[index].window.putIntoWindow()::class == NonBlockingOngoingWindow.WindowResponse.Success::class) {
+                                if (paymentServices[index].rateLimiter.tick()) {
+                                    nearestTimes[index] =
+                                        (System.currentTimeMillis() + (1.0 / paymentServices[index].getSpeed())).toLong()
+                                    paymentServices[index].submitPaymentRequest(
+                                        createdEvent.paymentId,
+                                        event.amount,
+                                        event.createdAt
+                                    )
+                                    break
+                                } else {
+                                    paymentServices[index].window.releaseWindow()
+                                }
+                            }
+                        } else
+                            for (index in paymentServices.indices) {
+                                if (paymentServices[index].window.putIntoWindow()::class == NonBlockingOngoingWindow.WindowResponse.Success::class) {
+                                    if (paymentServices[index].rateLimiter.tick()) {
+                                        nearestTimes[index] =
+                                            (System.currentTimeMillis() + (1.0 / paymentServices[index].getSpeed())).toLong()
+                                        paymentServices[index].submitPaymentRequest(
+                                            createdEvent.paymentId,
+                                            event.amount,
+                                            event.createdAt
+                                        )
+                                        break@outerCycle
+                                    } else {
+                                        paymentServices[index].window.releaseWindow()
+                                    }
+                                }
+                            }
+                    }
                 }
             }
         }
