088ada999d0f05e0c389c3fcbf0a974b96e7f18c
anasteshap <anasteshap@gmail.com>
2025-04-03 10:53:13 +0300

    fix

--- src/main/kotlin/ru/quipy/payments/config/PaymentAccountsConfig.kt
+++ src/main/kotlin/ru/quipy/payments/config/PaymentAccountsConfig.kt
@@ -3,6 +3,7 @@ package ru.quipy.payments.config
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import ru.quipy.core.EventSourcingService
@@ -18,17 +19,19 @@ import java.util.*
 @Configuration
 class PaymentAccountsConfig {
     companion object {
-        private val PAYMENT_PROVIDER_HOST_PORT: String = "localhost:1234"
         private val javaClient = HttpClient.newBuilder().build()
         private val mapper = ObjectMapper().registerKotlinModule().registerModules(JavaTimeModule())
     }
+
+    @Value("\${payment.hostPort}")
+    lateinit var paymentProviderHostPort: String
 
     private val allowedAccounts = setOf("acc-7")
 
     @Bean
     fun accountAdapters(paymentService: EventSourcingService<UUID, PaymentAggregate, PaymentAggregateState>): List<PaymentExternalSystemAdapter> {
         val request = HttpRequest.newBuilder()
-            .uri(URI("http://${PAYMENT_PROVIDER_HOST_PORT}/external/accounts?serviceName=onlineStore")) // todo sukhoa service name
+            .uri(URI("http://${paymentProviderHostPort}/external/accounts?serviceName=onlineStore")) // todo sukhoa service name
             .GET()
             .build()
 
