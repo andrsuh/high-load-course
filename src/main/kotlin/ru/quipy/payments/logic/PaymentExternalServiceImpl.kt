d67279454f1f0716028628fb54d098cf9e045237
LakeevGeorgiy <lakeev.gosha2016@yandex.ru>
2025-09-15 23:42:50 +0300

    fix online-shop: use SlidindWindowRateLimiter

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -6,6 +6,7 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
 import org.slf4j.LoggerFactory
+import ru.quipy.common.utils.SlidingWindowRateLimiter
 import ru.quipy.core.EventSourcingService
 import ru.quipy.payments.api.PaymentAggregate
 import java.net.SocketTimeoutException
@@ -33,6 +34,7 @@ class PaymentExternalSystemAdapterImpl(
     private val requestAverageProcessingTime = properties.averageProcessingTime
     private val rateLimitPerSec = properties.rateLimitPerSec
     private val parallelRequests = properties.parallelRequests
+    private val rate_limiter = SlidingWindowRateLimiter(10, java.time.Duration.ofMillis(1000));
 
     private val client = OkHttpClient.Builder().build()
 
@@ -48,6 +50,8 @@ class PaymentExternalSystemAdapterImpl(
         }
 
         logger.info("[$accountName] Submit: $paymentId , txId: $transactionId")
+
+        rate_limiter.tickBlocking()
 
         try {
             val request = Request.Builder().run {
