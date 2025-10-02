b060087f41e92f1846cf56be5f71c655fc8f2896
sssix6ix6ix <amatveev797@gmail.com>
2025-10-03 02:10:40 +0300

    .

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -1,6 +1,28 @@
 package ru.quipy.payments.logic
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.selects.select
+import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+
+import org.springframework.stereotype.Component
+
+
+import kotlinx.coroutines.launch
+
+import io.micrometer.core.annotation.Timed
+import io.micrometer.core.annotation.Counted
+import io.micrometer.core.instrument.Gauge
+import io.micrometer.core.instrument.Timer
+import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.MeterRegistry
+
+import java.util.concurrent.atomic.AtomicInteger
+
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -10,7 +32,7 @@ import ru.quipy.common.utils.CompositeRateLimiter
 import ru.quipy.common.utils.CountingRateLimiter
 import ru.quipy.common.utils.FixedWindowRateLimiter
 import ru.quipy.common.utils.SlidingWindowRateLimiter
-import ru.quipy.common.utils.OngoingWindow
+import ru.quipy.common.utils.NonBlockingOngoingWindow
 import ru.quipy.core.EventSourcingService
 import ru.quipy.payments.api.PaymentAggregate
 import java.net.SocketTimeoutException
@@ -25,7 +47,8 @@ class PaymentExternalSystemAdapterImpl(
     private val paymentESService: EventSourcingService<UUID, PaymentAggregate, PaymentAggregateState>,
     private val paymentProviderHostPort: String,
     private val token: String,
-) : PaymentExternalSystemAdapter {
+    private val meterRegistry: MeterRegistry,
+    ) : PaymentExternalSystemAdapter {
 
     companion object {
         val logger = LoggerFactory.getLogger(PaymentExternalSystemAdapter::class.java)
@@ -34,18 +57,71 @@ class PaymentExternalSystemAdapterImpl(
         val mapper = ObjectMapper().registerKotlinModule()
     }
 
+    private class PaymentTask(
+        val paymentId: UUID,
+        val amount: Int,
+        val paymentStartedAt: Long,
+        val deadline: Long,
+        val transactionId: UUID,
+        val sample: Timer.Sample
+    ) {
+        fun deadlineExpired() : Boolean {
+            return System.currentTimeMillis() >= deadline
+        }
+    }
+
+    private val paymentQueue = Channel<PaymentTask>(Channel.UNLIMITED)
+    private val paymentQueueSize = AtomicInteger(0)
+    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
     private val serviceName = properties.serviceName
     private val accountName = properties.accountName
     private val requestAverageProcessingTime = properties.averageProcessingTime
     private val rateLimitPerSec = properties.rateLimitPerSec
     private val parallelRequests = properties.parallelRequests
 
-    private val ongoingWindow = OngoingWindow(parallelRequests)
+    private val requestFailuredCounter = Counter.builder("payment_request_failed_count").description("Number of payments request failed").register(meterRegistry)
+    private val submissionCounter = Counter.builder("payment_submission_count").description("Number of payments submitted").register(meterRegistry)
+    private val processingTimer = Timer.builder("payment_processing_time").description("Time taken to process a payment").register(meterRegistry)
+    private val fullProcessingTimer = Timer.builder("payment_full_processing_time").description("Time taken to full process payment").register(meterRegistry)
+    private val successfulProcessingCounter = Counter.builder("payment_successful_processing_count").description("Number of payments successfully processed").register(meterRegistry)
+    
+    init {
+        // startQueueProcessing()
+        setupMetrics()
+    }
+
+    // private fun startQueueProcessing() {
+    //     repeat(parallelRequests) { _ ->
+    //         scope.launch {
+    //             while (true) {
+    //                 val task = paymentQueue.receive()
+    //                 paymentQueueSize.decrementAndGet()
+    //                 if (task.deadlineExpired()) {
+    //                     requestFailuredCounter.increment()
+    //                     logger.warn("[$accountName] Payment request deadline expired for payment ${task.paymentId}, txId: ${task.transactionId}")
+    //                     paymentQueueSize.decrementAndGet()
+    //                     task.sample.stop(fullProcessingTimer)
+    //                     continue
+    //                 }
+    //                 processPaymentTask(task.paymentId, task.amount, task.paymentStartedAt, task.deadline, task.transactionId, task.sample)
+    //             }
+    //         }
+    //     }
+    // }
+
+    private val ongoingWindow = NonBlockingOngoingWindow(parallelRequests)
     private val rateLimiterOneSecond = SlidingWindowRateLimiter(rateLimitPerSec.toLong(), Duration.ofSeconds(1))
 
     private val client = OkHttpClient.Builder().build()
 
+    private fun setupMetrics() {
+        Gauge.builder("payment_queue_size", paymentQueueSize){it -> it.get().toDouble()}.description("Size of the payment queue").register(meterRegistry)
+    }
+
     override fun performPaymentAsync(paymentId: UUID, amount: Int, paymentStartedAt: Long, deadline: Long) {
+        submissionCounter.increment()
+        paymentQueueSize.incrementAndGet()
         logger.warn("[$accountName] Submitting payment request for payment $paymentId")
 
         val transactionId = UUID.randomUUID()
@@ -57,17 +133,42 @@ class PaymentExternalSystemAdapterImpl(
         }
 
         logger.info("[$accountName] Submit: $paymentId , txId: $transactionId")
+        // val task = PaymentTask(paymentId, amount, paymentStartedAt, deadline, transactionId, Timer.start())
+        // scope.launch {
+        //     paymentQueue.send(task)
+        //     paymentQueueSize.incrementAndGet()
+        //     logger.info("[$accountName] Submitted payment request for payment $paymentId")
+        // }
+        runBlocking { processPaymentTask(paymentId, amount, paymentStartedAt, deadline, transactionId, Timer.start()) }
+    }
 
+    private suspend fun processPaymentTask(paymentId: UUID, amount: Int, paymentStartedAt: Long, deadline: Long, transactionId: UUID, sampleFullProcessing: Timer.Sample) {
+        val sample = Timer.start()
+        logger.info("[$accountName] Processing payment request for payment $paymentId, txId: $transactionId")
+        var needToReleaseWindow: Boolean = false
         try {
-            ongoingWindow.acquire()
-            rateLimiterOneSecond.tickBlocking()
-            
+            val low_bound_of_timeout = deadline - now() - requestAverageProcessingTime.toMillis()
+            if (low_bound_of_timeout <= 0) {
+                requestFailuredCounter.increment()
+                logger.warn("[$accountName] Request timeout for payment $paymentId, txId: $transactionId")
+                throw Exception("Request timeout")
+            }
+            needToReleaseWindow = true
+
+            ongoingWindow.putIntoWindow()
+            while (!rateLimiterOneSecond.tick()) {}
+            logger.info("[$accountName] Acquired semaphore and rate limiter for payment $paymentId, txId: $transactionId")
             val request = Request.Builder().run {
                 url("http://$paymentProviderHostPort/external/process?serviceName=$serviceName&token=$token&accountName=$accountName&transactionId=$transactionId&paymentId=$paymentId&amount=$amount")
                 post(emptyBody)
             }.build()
 
-            client.newCall(request).execute().use { response ->
+            val clientWithTimeout = client
+                    .newBuilder()
+                    .callTimeout(Duration.ofMillis(deadline - now()))
+                    .build()
+
+            clientWithTimeout.newCall(request).execute().use { response ->
                 val body = try {
                     mapper.readValue(response.body?.string(), ExternalSysResponse::class.java)
                 } catch (e: Exception) {
@@ -75,6 +176,7 @@ class PaymentExternalSystemAdapterImpl(
                     ExternalSysResponse(transactionId.toString(), paymentId.toString(),false, e.message)
                 }
 
+                successfulProcessingCounter.increment()
                 logger.warn("[$accountName] Payment processed for txId: $transactionId, payment: $paymentId, succeeded: ${body.result}, message: ${body.message}")
 
                 // Здесь мы обновляем состояние оплаты в зависимости от результата в базе данных оплат.
@@ -101,7 +203,12 @@ class PaymentExternalSystemAdapterImpl(
                 }
             }
         } finally {
-            ongoingWindow.release()
+            if (needToReleaseWindow) {
+                ongoingWindow.releaseWindow()
+            }
+            paymentQueueSize.decrementAndGet()
+            sample.stop(processingTimer)
+            sampleFullProcessing.stop(fullProcessingTimer)
         }
     }
 
