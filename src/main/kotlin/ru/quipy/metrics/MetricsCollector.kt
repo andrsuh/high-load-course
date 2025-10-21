50ec92f050df2016b5e406aea49a78299ae39d08
polikiss <mymmypolly@gmail.com>
2025-10-21 20:13:03 +0300

    metric

--- src/main/kotlin/ru/quipy/metrics/MetricsCollector.kt
+++ src/main/kotlin/ru/quipy/metrics/MetricsCollector.kt
@@ -34,13 +34,12 @@ class MetricsCollector {
             .increment()
     }
 
-    private val tooManyRequestsCounter = Counter
-        .builder("too_many_requests")
-        .description("Number of failed requests")
-        .register(Metrics.globalRegistry)
-
     fun status429RequestInc() {
-        tooManyRequestsCounter.increment()
+        Counter
+            .builder("too_many_requests")
+            .description("Number of failed requests")
+            .register(Metrics.globalRegistry)
+            .increment()
     }
     fun failedRequestExternalInc(account: String) {
         Counter
