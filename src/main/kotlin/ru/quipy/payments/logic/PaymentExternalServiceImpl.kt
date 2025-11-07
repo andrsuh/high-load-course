ca2bd0a2b2a074e060e9fbd317f7e5011be6966c
Mikhail <polyakov.mihail@skbkontur.ru>
2025-11-07 11:40:18 +0300

    hw 7 research

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -2,13 +2,13 @@ package ru.quipy.payments.logic
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.MeterRegistry
+import io.prometheus.metrics.core.metrics.Summary
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
-import io.micrometer.core.instrument.Counter
-import io.micrometer.core.instrument.MeterRegistry
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import ru.quipy.common.utils.OngoingWindow
 import ru.quipy.common.utils.SlidingWindowRateLimiter
 import ru.quipy.core.EventSourcingService
@@ -46,9 +46,17 @@ class PaymentExternalSystemAdapterImpl(
     private val ongoingWindow = OngoingWindow(parallelRequests)
 
     private val submittedCounter = Counter.builder("payments_submitted_total").register(meterRegistry)
-    private val rejectedGlobalLimiterCounter = Counter.builder("payments_rejected_global_limiter_total").register(meterRegistry)
     private val sentQueriesSuccess = Counter.builder("payments_success").register(meterRegistry)
-    private val sentQueriesTotal = Counter.builder("payments_queries_total").register(meterRegistry)
+
+    var requestLatency: Summary = Summary.builder()
+        .name("request_latency")
+        .help("Request latency.")
+        .quantile(0.5, 0.01)
+        .quantile(0.8, 0.005)
+        .quantile(0.99, 0.005)
+        .labelNames("status_code")
+        .register()
+
 
     private fun waitRateLimitOrTimeout(deadline: Long): Boolean {
         while (!rateLimiter.tick()) {
@@ -94,7 +102,11 @@ class PaymentExternalSystemAdapterImpl(
                     post(emptyBody)
                 }.build()
 
+                val start = System.currentTimeMillis()
                 client.newCall(request).execute().use { response ->
+                    val latency = (System.currentTimeMillis() - start).toDouble()
+                    requestLatency.observe(latency)
+
                     val body = try {
                         mapper.readValue(response.body?.string(), ExternalSysResponse::class.java)
                     } catch (e: Exception) {
