3f19c9e6c7d545bbeb4a24374322e742f95ef2a0
Искандер Вильданов <i-vildanov@inbox.ru>
2025-12-05 11:11:52 +0300

    погнал 9

--- src/main/kotlin/ru/quipy/payments/api/GlobalExceptionHandler.kt
+++ src/main/kotlin/ru/quipy/payments/api/GlobalExceptionHandler.kt
@@ -1,5 +1,7 @@
 package ru.quipy.payments.api
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -9,8 +11,14 @@ import ru.quipy.payments.exceptions.TooManyRequestsException
 @ControllerAdvice
 class GlobalExceptionHandler {
 
+    val logger: Logger = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
+
     @ExceptionHandler(TooManyRequestsException::class)
     fun handleTooManyRequests(ex: TooManyRequestsException): ResponseEntity<String> {
+
+        logger.error("Exception message: ${ex.message} . We will retry after ${ex.retryAfterSeconds}")
+        logger.error(ex.message)
+
         return ResponseEntity
             .status(HttpStatus.TOO_MANY_REQUESTS)
             .header("Retry-After", ex.retryAfterSeconds.toString())
